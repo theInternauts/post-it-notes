a7da0eeef436504fce636e61862c817f390557f0
theInternauts <cricketw@wearetheinternauts.com>
2014-02-05 02:34:45 -0800

    converted MainBoardView view into a module

---
+++ js/views/mainBoard.js
@@ -0,0 +1,47 @@
+define( [ 'PostBoard', 'jquery', 'underscore', 'backbone', 'jquery-ui'], function( PostBoard, $, _, Backbone ){
+	PostBoard.Views.MainBoard = Backbone.View.extend({
+		events: {
+	    	'click body': 'addPostItHandler'
+		},
+		defaultPosition: {
+			top: 50,
+			left: 50
+		},
+		render: function(){
+			this.setElement('html')
+			this.$('body').attr('id', 'board')
+			this.$('body').append(new PostBoard.Views.Toolbar().render().el)
+			return this
+		},
+		addPostItHandler: function(event){
+			event.stopPropagation()
+			var position = { top: event.pageY, left: event.pageX }
+	    	this.allPostModels.add(new PostBoard.Models.PostIt({ id: event.timeStamp.toString(), position: position }))
+		},
+		addPostIt: function(post_model){
+			var post_model = post_model
+			!post_model.get('id') ? post_model.set('id', Date.now().toString()) : null
+			!post_model.get('position') ? post_model.set('position', this.defaultPosition) : null
+			var newView = new PostBoard.Views.PostItView({ model: post_model, collection: this.allPostModels })
+			this.allPostViews[post_model.get('id')] = newView;
+			this.$('body').append(newView.render().$el.css(post_model.get('position')).attr('id',post_model.get('id')).draggable({ handle: '.header-label' }))
+			return this
+		},
+		removePostIt: function(post_model){
+			var id = post_model.get('id')
+			console.log('#'+ id)
+			this.$('#'+id).draggable('destroy')
+			this.allPostViews[id].remove()
+			delete this.allPostViews[id]
+		},
+		initialize: function(){
+			this.allPostModels = new PostBoard.Collections.PostItCollection()
+			this.allPostViews = {}
+			this.$('body').on('click', this.addPostItHandler)
+			this.allPostModels.on('add', this.addPostIt, this)
+			this.allPostModels.on('remove', this.removePostIt, this)
+		}
+	})
+
+	return PostBoard.Views.MainBoard
+})
