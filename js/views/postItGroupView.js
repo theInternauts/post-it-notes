96e32cbe4a01714607158531a6cfd5cb13744211
theInternauts <cricketw@wearetheinternauts.com>
2014-02-06 23:57:18 -0800

    created BASE post-it group view

---
+++ js/views/postItGroupView.js
@@ -0,0 +1,15 @@
+define( [ 'PostBoard', 'jquery', 'underscore', 'backbone', 'jquery-ui'], function( PostBoard, $, _, Backbone ){
+	PostBoard.Views.PostItGroupView = Backbone.View.extend({
+		attributes: { 
+			class: 'post-it-group',
+			style: 'display:block'
+		},
+		template: _.template('<div class=><div class="header"><a>x</a><div class="header-label" contenteditable="true"><%= get("header") %></div></div><div class="content"><%= get("content") %></div>'),
+		render: function () {
+		    this.$el.html(this.template(this.model))
+			return this
+		}		
+	})
+	
+	return PostBoard.Views.PostItGroupView
+})
