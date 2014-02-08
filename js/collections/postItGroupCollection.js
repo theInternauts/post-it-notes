adee6778b46728d721fce846c1fa9cbb2a82d3f4
theInternauts <cricketw@wearetheinternauts.com>
2014-02-08 01:44:29 -0800

    refactored to simplify and remove unneded dependency declarations

--- js/collections/postItGroupCollection.js
+++ js/collections/postItGroupCollection.js
@@ -1,7 +1,6 @@
 define( ['PostBoard', 'underscore', 'backbone', 'models/postItGroup'], function ( PostBoard, _, Backbone, postItGroup ) {
-	PostBoard.Models.PostItGroup = postItGroup
 	PostBoard.Collections.PostItGroupCollection = Backbone.Collection.extend({
-		model: PostBoard.Models.PostIt
+		model: postItGroup
 	})
 
 	return PostBoard.Collections.PostItGroupCollection
