8b5baf5f8533b001ea3b56a713283803aa94025b
theInternauts <cricketw@wearetheinternauts.com>
2014-02-08 00:53:51 -0800

    adding default values to the PostIt model

--- js/models/postIt.js
+++ js/models/postIt.js
@@ -1,5 +1,11 @@
 define( ['PostBoard', 'underscore', 'backbone'], function ( PostBoard, _, Backbone ) {
-	PostBoard.Models.PostIt = Backbone.Model.extend({})
+	PostBoard.Models.PostIt = Backbone.Model.extend({
+		defaults: {
+			header: '',
+			id: Date.now().toString(),
+			content: ''
+		}
+	})
 
 	return PostBoard.Models.PostIt
 })
