54e4b402dba503490961f1ec676f77ca9f0ceefd
theInternauts <cricketw@wearetheinternauts.com>
2014-02-08 01:47:25 -0800

    REFACTORED to simplify and allow for the MainBoard to pass in an appropriate model upon instantiation

--- js/views/postItGroupView.js
+++ js/views/postItGroupView.js
@@ -4,15 +4,10 @@ define( [ 'PostBoard', 'jquery', 'underscore', 'backbone', 'jquery-ui'], functio
 			class: 'post-it-group',
 			style: 'display:block'
 		},
-		template: _.template('<div class=><div class="header"><a>x</a><div class="header-label" contenteditable="true"><%= get("header") %></div></div><div class="content"><%= get("content") %></div>'),
+		template: _.template('<div class="header"><a>x</a><div class="header-label" contenteditable="true"><%= get("header") %></div></div><div class="content"></div>'),
 		render: function () {
 		    this.$el.html(this.template(this.model))
 			return this
-		},
-		initialize: function () {
-			this.collection = Backbone.Collection.extend({
-				model: PostBoard.Views.PostIt
-			})			
 		}		
 	})
 	
