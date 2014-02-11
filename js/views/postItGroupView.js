063bf7b9b4c969d127546595b52a4414930238d6
theinternauts <cricketw@wearetheinternauts.com>
2014-02-10 17:04:17 -0800

    added drop behavior to the PostItGroupView

--- js/views/postItGroupView.js
+++ js/views/postItGroupView.js
@@ -7,7 +7,16 @@ define( [ 'PostBoard', 'jquery', 'underscore', 'backbone', 'jquery-ui'], functio
 		template: _.template('<div class="header"><a>x</a><div class="header-label" contenteditable="true"><%= get("header") %></div></div><div class="content"></div>'),
 		render: function () {
 		    this.$el.html(this.template(this.model))
-
+		    this.$el.children('.content').droppable({
+		    	accept: '.post-it',
+		    	snap: 'div',
+		    	snapMode: 'outer',
+		    	drop: function(event, ui){
+		    		console.log(event, ui)
+		    		ui_node = ui.draggable.css({ 'position':'static', 'margin':'0 auto 10px auto'})
+		    		$(event.target).append(ui_node)
+		    	}
+		    })
 			return this
 		}
 	})
